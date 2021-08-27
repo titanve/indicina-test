e086bca0ac4aeded5ea4aa248b641d5867e76c8e
Rafael Mora <rafael.mora.guti@gmail.com>
2021-08-27 12:47:02 -0500

    Request the code and access_token and save them

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,40 @@ import React from "react";
 import "./App.css";
 import LoginGithub from "react-login-github";
 
-const onSuccess = (response: string) => {console.log(response)};
-const onFailure = (response: string) => console.error(response);
+const API_Auth_Backend: string =
+  "https://9uj0ihoex6.execute-api.eu-west-1.amazonaws.com/dev/auth";
+
+interface AuthResponse {
+  code: string;
+}
 
 function App() {
+  const [code, setCode] = React.useState("");
+  const [access_token, setAccess_token] = React.useState("");
+
+  const onSuccess = (response: AuthResponse) => {
+    setCode(response?.code);
+  };
+
+  React.useEffect(() => {
+    const fetchAccessToken = async () => {
+      const response = await fetch(API_Auth_Backend, {
+        method: "POST",
+        body: JSON.stringify({ code: code }),
+      });
+      const { data } = await response.json();
+      if (response.ok) {
+        setAccess_token(data?.access_token);
+      }
+    };
+
+    if (code != null) {
+      fetchAccessToken();
+    }
+  }, [code]);
+
+  const onFailure = (response: string) => console.error(response);
+
   return (
     <div className="App">
       <main className="App-main">
