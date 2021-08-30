973ab2b90af5638ca1c0da4e2e84b6f2bef03d58
Rafael Mora <rafael.mora.guti@gmail.com>
2021-08-29 20:34:26 -0500

    Improve styling for UserItem and put the rest of the fields

--- src/components/UserItem.tsx
+++ src/components/UserItem.tsx
@@ -3,8 +3,8 @@ import "../App.css";
 
 interface Node {
   name: string;
-  description: string;
-  [key:string]: string;
+  bio: string;
+  login: string;
 }
 
 interface User {
@@ -12,11 +12,16 @@ interface User {
   node: Node;
 }
 
-const UserItem: React.FC<{ user: User }> = (props) =>{
-  return <div className="App-Results-detail">
-    <p>{props.user.node.name}</p>
-    <p>{props.user.node.description}</p>
-  </div>;
-}
+const UserItem: React.FC<{ user: User }> = (props) => {
+  return (
+    <div className="App-Results-detail">
+      <div className="App-UserItem-header">
+        <div className="App-UserItem-name">{props.user.node.name}</div>
+        <div className="App-UserItem-login">{props.user.node.login}</div>
+      </div>
+      <div className="App-UserItem-bio">{props.user.node.bio}</div>
+    </div>
+  );
+};
 
 export { UserItem };
