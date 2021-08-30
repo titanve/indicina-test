477abb815015dad6ad1b9996206b1ff765a90094
Rafael Mora <rafael.mora.guti@gmail.com>
2021-08-30 07:55:15 -0500

    Improve router and tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,34 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event'
-import {createMemoryHistory} from 'history'
-import {Router} from 'react-router-dom'
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { createMemoryHistory } from "history";
+import { Router } from "react-router-dom";
 
-import '@testing-library/jest-dom'
+import "@testing-library/jest-dom";
 
-import {App} from './App';
+import { App } from "./App";
 
-test('renders Login to Github button', () => {
-  render(<App />);
+test("renders Login to Github button", () => {
+  const history = createMemoryHistory();
+  render(
+    <Router history={history}>
+      <App />
+    </Router>
+  );
   const buttonElement = screen.getByText(/login to github/i);
   expect(buttonElement).toBeInTheDocument();
 });
 
+test("landing on a bad page", () => {
+  const history = createMemoryHistory();
+
+  render(
+    <Router history={history}>
+      <App />
+    </Router>
+  );
+
+  history.push("/some/bad/route");
+  expect(history.length).toBe(2);
+  expect(history.location.pathname).toBe("/some/bad/route");
+  expect(screen.getByText(/no route match/i)).toBeInTheDocument();
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 import { PrivateRoute } from "./components/PrivateRoute";
 import { Login } from "./components/Login";
 import { Search } from "./components/Search";
@@ -8,32 +8,31 @@ import { Results } from "./components/Results";
 const NoMatch = () => (
   <div className="App-no-match">
     <div className="App-no-match-arrange">
-    <p>No route match</p>
-    <p>
-      Go to <Link to="/">Home</Link>
-    </p></div>
+      <p>No route match</p>
+      <p>
+        Go to <Link to="/">Home</Link>
+      </p>
+    </div>
   </div>
 );
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Login />
-        </Route>
-        <PrivateRoute path="/search">
-          <Search />
-        </PrivateRoute>
-        <PrivateRoute path="/results">
-          <Results />
-        </PrivateRoute>
+    <Switch>
+      <Route exact path="/">
+        <Login />
+      </Route>
+      <PrivateRoute path="/search">
+        <Search />
+      </PrivateRoute>
+      <PrivateRoute path="/results">
+        <Results />
+      </PrivateRoute>
 
-        <Route>
-          <NoMatch />
-        </Route>
-      </Switch>
-    </Router>
+      <Route>
+        <NoMatch />
+      </Route>
+    </Switch>
   );
 }
 
